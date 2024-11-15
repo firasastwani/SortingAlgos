0512711e79d943c14130fb0c3e8c5ea44673ae55
firasastwani <firasastwani1@gmail.com>
2024-11-15 14:59:16 -0500

    exp 1 and 2 drivers

--- SortDriverEX1.java
+++ SortDriverEX1.java
@@ -3,10 +3,9 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 import java.util.Arrays;
 
-public class SortDriver {
-
-    // Experiment 1
-    /*
+public class SortDriverEX1 {
+   
+     // Experiment 1
     public static void main(String[] args) {
         if (args.length < 1) {
             System.out.println("No inputted file");
@@ -60,9 +59,9 @@ public class SortDriver {
         System.out.println("#" + choice + "-sort comparisons: " + comparisons);
         scanner.close(); 
     }
-    */
+    
 
-    /* 
+    
     // Experiment 1
     // Puts the numbers from the inputted file into an array
     private static int[] readNumbersFromFile(String filename) {
@@ -87,58 +86,5 @@ public class SortDriver {
 
         return null;
     }
-    */
 
-
-    // Experiment 2
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Enter the number of random values to generate: ");
-        int n = scanner.nextInt();
-
-        int[] numbers = new int[n];
-        for (int i = 0; i < n; i++) {
-            numbers[i] = (int) (Math.random() * 10000); // Random values between 0 and 9999
-        }
-
-        System.out.println("selection-sort (s) merge-sort (m) heap-sort (h) quick-sort-fp (q) quick-sort-rp (r)");
-        System.out.print("Enter the algorithm: ");
-
-        String choice = scanner.next();
-        Sorting sorting = new Sorting();
-
-        long comparisons = 0;
-
-        switch (choice) {
-            case "s":
-                comparisons = sorting.selectionSort(numbers);
-                break;
-            case "m":
-                comparisons = sorting.mergeSort(numbers);
-                break;
-            case "h":
-                comparisons = sorting.heapSort(numbers);
-                break;
-            case "q":
-                comparisons = sorting.quickSort(numbers);
-                break;
-            case "r":
-                comparisons = sorting.randomQuickSort(numbers);
-                break;
-            default:
-                System.out.println("Invalid choice");
-                scanner.close();
-                return;
-        }
-
-        // Testing the sorted array
-        //System.out.println("Sorted array: " + Arrays.toString(numbers));
-
-        System.out.println("#" + choice + "-sort comparisons: " + comparisons);
-        scanner.close();
-    }
-
-    
 }
-
---
+++ SortDriverEX2.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+
+public class SortDriverEX2 {
+
+
+    // Experiment 2
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of random values to generate: ");
+        int n = scanner.nextInt();
+
+        int[] numbers = new int[n];
+        for (int i = 0; i < n; i++) {
+            numbers[i] = (int) (Math.random() * 10000); // Random values between 0 and 9999
+        }
+
+        System.out.println("selection-sort (s) merge-sort (m) heap-sort (h) quick-sort-fp (q) quick-sort-rp (r)");
+        System.out.print("Enter the algorithm: ");
+
+        String choice = scanner.next();
+        Sorting sorting = new Sorting();
+
+        long comparisons = 0;
+
+        switch (choice) {
+            case "s":
+                comparisons = sorting.selectionSort(numbers);
+                break;
+            case "m":
+                comparisons = sorting.mergeSort(numbers);
+                break;
+            case "h":
+                comparisons = sorting.heapSort(numbers);
+                break;
+            case "q":
+                comparisons = sorting.quickSort(numbers);
+                break;
+            case "r":
+                comparisons = sorting.randomQuickSort(numbers);
+                break;
+            default:
+                System.out.println("Invalid choice");
+                scanner.close();
+                return;
+        }
+
+        // Testing the sorted array
+        //System.out.println("Sorted array: " + Arrays.toString(numbers));
+
+        System.out.println("#" + choice + "-sort comparisons: " + comparisons);
+        scanner.close();
+    }
+
+    
+}
+
